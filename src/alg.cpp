23f241d524f3c7bcefa97872822acd42b80abb21
vidikson <65093952+vidikson@users.noreply.github.com>
2021-03-09 10:53:08 +0300

    Кутузова Виктория

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,8 @@
 // Copyright 2021 NNTU-CS
 
-int gcd(int a, int b) {
-       while (a!=b)
-    {
-    if (a>b) a-=b;
-    else b-=a;
-    }
-    return a;
+int gcd(int a, int b)
+{
+    if (b == 0) 
+           return a; 
+       return gcd(b, a % b);
 }
